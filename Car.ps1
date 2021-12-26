45db0b42e61d4deb67f267782af0bcbf192417d2
Avamov Farukh <avamov.farukh@gmail.com>
2021-12-26 20:45:57 +0600

    Almost done

--- Car.ps1
+++ Car.ps1
@@ -1,13 +1,13 @@
 Class Car {
-    [string] $Brand
-    [string] $Model
+    [String] $Brand
+    [String] $Model
     [int] $DoorsAmount
     [double] $FuelConsumption
     [double] $CurrentFuel
 
     Car (
         [String] $Brand,
-        [string] $Model,
+        [String] $Model,
         [int] $DoorsAmount,
         [double] $FuelConsumption,
         [double] $CurrentFuel
@@ -21,9 +21,15 @@ Class Car {
     }
 
     [String] ToString(){
-        return "brand = $($this.Brand),model = $($this.Model),doors amount = $($this.DoorsAmount),fuel consumption = $($this.FuelConsumption),current fuel = $($this.CurrentFuel)"
+        return "
+        brand = $($this.Brand),
+        model = $($this.Model),
+        doors amount = $($this.DoorsAmount),
+        fuel consumption = $($this.FuelConsumption),
+        current fuel = $($this.CurrentFuel)
+        "
     }
-    [string]KmLeft(){
+    [String]KmLeft(){
         if ($this.CurrentFuel -le 0 -or $this.FuelConsumption -le 0) {
             return "Wrond data"
         } else {
@@ -31,18 +37,29 @@ Class Car {
         }
     }
 
-    [string]HoursLeft([int]$HoursPerKm){
-        $kmToSec =[timespan]::FromSeconds([math]::Round($this.CurrentFuel / $this.FuelConsumption * 100, 2)/$HoursPerKm * 60 * 60)
-        return $kmToSec.ToString("hh' hours 'mm' minutes 'ss' seconds'")
+    [String]HoursLeft([int]$KmPerHour){
+        $kmToSec =[timespan]::FromSeconds([math]::Round($this.CurrentFuel / $this.FuelConsumption * 100, 2)/$KmPerHour * 60 * 60)
+        return $kmToSec.ToString("hh' hours 'mm' minutes'")
 
     }
 }
-$audiCar = [Car]::new("Audi", "A5s", 3, 10.1, 50)
-$audiCar.ToString()
 
-# $newCat = [car]::new()
-# $newCat.FuelConsumption = 10
-# $newCat.CurrentFuel = 60
-# $newCat.KmLeft()
-# $newCat.HoursLeft(100)
+Class TwoDoorCar : Car {
+    TwoDoorCar(
+        [String] $Brand,
+        [String] $Model,
+        [double] $FuelConsumption,
+        [double] $CurrentFuel
+    ) : base($Brand, $Model, 2, $FuelConsumption, $CurrentFuel){
+    }
+}
+
+
+[Car]$newCar = [TwoDoorCar]::new("Audi", "RS5", 10.2, 50.5)
+
+$json = ConvertTo-Json($newCar);
+Invoke-WebRequest 127.0.0.1 -Body $json -Method Post -UseBasicParsing
+
+
+
 
